e06b555399c4a531889f45858429c6760a38d532
kouya0205 <kouya0205game@gmail.com>
2024-12-31 11:51:43 +0900

    auth終わりました。

--- src/app/company/auth/check-invite-email/page.tsx
+++ src/app/company/auth/check-invite-email/page.tsx
@@ -1,4 +1,6 @@
+import { Button } from '@/components/ui/button';
 import { createClient } from '@/utils/supabase/server';
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
 
 export default async function CheckInviteEmailPage() {
@@ -8,11 +10,14 @@ export default async function CheckInviteEmailPage() {
   } = await supabase.auth.getUser();
 
   if (user) {
-    redirect('/user/dashboard');
+    redirect('/company/dashboard');
   }
   return (
     <div>
       <h1>Check Invite Email</h1>
+      <Link href="/company/auth">
+        <Button>ログインへ</Button>
+      </Link>
     </div>
   );
 }
