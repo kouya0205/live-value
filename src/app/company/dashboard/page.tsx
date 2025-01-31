2f4ddebfe4a5ec629fa7021afe6a6e7a01ce6263
attack10 <atkymd10@gmail.com>
2025-01-31 21:22:30 +0900

    test2

--- src/app/company/dashboard/page.tsx
+++ src/app/company/dashboard/page.tsx
@@ -60,67 +60,61 @@ export default async function CompanyHome() {
             <h1 className="text-3xl font-bold mb-4">
               人材情報
             </h1>
-            <div>
               {articles.data.contents.map((article) => (
-                <Card className="p-6 mb-8">
-                  <div className="mb-6">
-                    <h2 className="text-xl font-bold mb-2">{article.user.name}</h2>
-                    <div className='flex'>
-                      <div className='w-[480]'>
-                        <Card className='my-4'>
-                          <div className='font-bold p-2'>基本情報</div>
-                          <div className='p-2'>
-                            <div className='flex gap-20'>
-                              <div>
-                                <span>年齢：</span>
-                              </div>
-                              <div>
-                                <span>希望勤務年数：</span>
-                              </div>
-                            </div>
-                            <div className='flex gap-20'>
-                              <div>
-                                <span>希望年収：</span>
-                              </div>
-                              <div>
-                                <span>希望勤務地：</span>
+                <div id='article.id'>
+                  <Card className="p-6 mb-8">
+                    <div className="mb-6">
+                      <h2 className="text-xl font-bold mb-2">{article.user.name}</h2>
+                      <div className='flex'>
+                        <div className='w-[480]'>
+                          <Card className='my-4'>
+                            <div className='font-bold p-2'>基本情報</div>
+                            <div className='p-2'>
+                              <div className='flex gap-28'>
+                                <div>
+                                  <div>年齢：</div>
+                                  <div>希望年収：</div>
+                                </div>
+                                <div>
+                                  <div>希望勤務年数：</div>
+                                  <div>希望勤務地：</div>
+                                </div>
                               </div>
                             </div>
-                          </div>
-                        </Card>
-                        <Card>
-                          <div className='font-bold'>スキルテスト結果</div>
-                          <div className='bg-slate-500 py-20 mb-4' />
-                        </Card>
-                      </div>
-                      <div>
-                        <Link href={`${article.noteUrl}`} key={article.id}>
-                          <Card className='w-full m-4'>
-                            <Image
-                              src={article.eyecatch || '/placeholder.svg'}
-                              alt={article.name}
-                              className="object-cover transition-transform group-hover:scale-105 m-4 rounded-lg"
-                              width={600}
-                              height={200}
-                            />
-                            <div className='text-xl font-bold m-2'>{article.name}</div>
                           </Card>
-                        </Link>
+                          <Card>
+                            <div className='font-bold'>スキルテスト結果</div>
+                            <div className='bg-slate-500 py-20 mb-4' />
+                          </Card>
+                        </div>
+                        <div>
+                          <Link href={`${article.noteUrl}`} key={article.id}>
+                            <Card className='w-full m-4'>
+                              <Image
+                                src={article.eyecatch || '/placeholder.svg'}
+                                alt={article.name}
+                                className="object-cover transition-transform group-hover:scale-105 m-4 rounded-lg"
+                                width={600}
+                                height={200}
+                              />
+                              <div className='text-xl font-bold m-2'>{article.name}</div>
+                            </Card>
+                          </Link>
+                        </div>
+                      </div>
+
+                      <div className='flex gap-2'>
+                        <Button className="w-full bg-white hover:bg-slate-200 text-black text-left justify-between" size="lg">
+                          詳細を見る
+                        </Button>
+                        <Button className="w-full bg-white hover:bg-slate-200 text-black text-left justify-between" size="lg">
+                          選考オファーを出す
+                        </Button>
                       </div>
                     </div>
-
-                    <div className='flex gap-2'>
-                      <Button className="w-full bg-red-700 hover:bg-red-800 text-white text-left justify-between" size="lg">
-                        詳細を見る
-                      </Button>
-                      <Button className="w-full bg-red-700 hover:bg-red-800 text-white text-left justify-between" size="lg">
-                        選考オファーを出す
-                      </Button>
-                    </div>
-                  </div>
-                </Card>
-              ))}
-            </div>
+                  </Card>
+                </div>
+                ))}
           </div>
         </div>
       </main>
